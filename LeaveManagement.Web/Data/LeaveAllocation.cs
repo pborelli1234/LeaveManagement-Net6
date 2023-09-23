cff12ce351f52865ead0c4f64e34244de8783420
Penny Borelli <Penny.Borelli@supersport.com>
2023-09-23 15:49:17 +0200

    Added Database Tables and Migrations. End Implement Database Section.

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        //public int Id { get; set; }
+          
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public int NumberOfDay { get; set; }
+        //public DateTime DateCreated { get; set; }
+        //public DateTime DateModified { get; set; }
+
+    }
+}
